ebe172be3c0c3f59507451b3d131359e5dd26add
Sarah Zhao <sarahzhao25@gmail.com>
2018-03-21 21:16:29 -0400

    added colors to city points

--- client/src/Board.jsx
+++ client/src/Board.jsx
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { cities } from './utils/cities';
+import L from 'leaflet';
 
 const stamenTonerTiles = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png';
 const basicTiles = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const stamenTonerAttr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+
+let BallIcon = L.Icon.extend({
+  options: {
+      iconSize: [30, 30],
+  }
+});
+
+let iconContainer = {
+  redIcon: new BallIcon({iconUrl: 'https://lh5.ggpht.com/JUGn9I-kMM3LriNMpdUA6Z1_NZksTHCndCJ7SqSG0CkF6P-rBHUS91_aAiWfNpKSoQ=w300'}),
+  blueIcon: new BallIcon({iconUrl: 'http://lobelpost.com/v17/files/stacks-image-12a7505.png'}),
+  yellowIcon: new BallIcon({iconUrl: 'https://cdn.pixabay.com/photo/2013/07/12/15/29/ball-149922_1280.png'}),
+  blackIcon: new BallIcon({iconUrl: 'https://totalsororitymove.com/wp-content/uploads/user_avatars/blackball.png'})
+};
 
 export default class Board extends Component {
   componentDidMount() {
@@ -18,6 +32,7 @@ export default class Board extends Component {
   render() {
     const center = [0,0];
     const zoomLevel = 1;
+    //let {redIcon, blueIcon, yellowIcon, blackIcon} = iconContainer;
     return (
       <div>
         <Map
@@ -29,11 +44,10 @@ export default class Board extends Component {
           maxBounds={[[70,-100],[-60,120]]}
         >
           <TileLayer
-            url={basicTiles}
+            url={stamenTonerTiles}
           />
-          <Marker position={[0,0]} />
           {
-            cities.map((city, index) => <Marker position={city.coords} key={index}/>)
+            cities.map((city, index) => <Marker position={city.coords} key={index} icon={iconContainer[city.icon]}/>)
           }
         </Map>
       </div>

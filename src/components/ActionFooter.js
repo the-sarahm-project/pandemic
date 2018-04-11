30ebf07f4e5791a342bb22154a3ab03e13ce483b
Sam Chai <sam.chai0501@gmail.com>
2018-04-11 19:50:57 -0400

    add icons to footer

--- src/components/ActionFooter.js
+++ src/components/ActionFooter.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import { Sidebar } from 'semantic-ui-react';
+import { Sidebar, Icon } from 'semantic-ui-react';
 
 const ActionFooter = () => {
   return (
-    <Sidebar className="action-footer" direction="bottom" visible={true} width="very wide" />
+    <Sidebar className="action-footer" direction="bottom" visible={true} width="very wide">
+      <div className="action-container">
+        <div className="move">
+          <Icon name="car" size="big" />/
+          <Icon name="plane" size="big" />
+        </div>
+        <div className="build">
+          <Icon name="building" size="big" />
+        </div>
+        <div className="share">
+          <Icon name="gift" size="big" />
+        </div>
+        <div className="treat">
+          <Icon name="medkit" size="big" />
+        </div>
+        <div className="cure">
+          <Icon name="lab" size="big" />
+        </div>
+      </div>
+    </Sidebar>
   );
 };
 

d86097fcc739ec0c57b089bc9c007407ed37ddd3
Sam Chai <sam.chai0501@gmail.com>
2018-04-11 19:09:00 -0400

    add ActionFooter component

---
+++ src/components/ActionFooter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Sidebar } from 'semantic-ui-react';
+
+const ActionFooter = () => {
+  return (
+    <Sidebar direction="bottom" />
+  );
+};
+
+export default ActionFooter;

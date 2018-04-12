65f362467942d60e61fd4333d72ed10b0d66810c
Sam Chai <sam.chai0501@gmail.com>
2018-04-12 12:08:40 -0400

    add GameHeader

---
+++ src/components/GameHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Sidebar, Icon, Button } from 'semantic-ui-react';
+
+const GameHeader = () => {
+  return (
+    <Sidebar className="game-header" direction="top" visible={true} width="very wide">aweorjieiajrioeajr
+    </Sidebar>
+  );
+};
+
+export default GameHeader;

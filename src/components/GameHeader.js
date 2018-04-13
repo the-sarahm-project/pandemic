0711a58e24dd120610df532a71d7d2f6bfcff36d
Sam Chai <sam.chai0501@gmail.com>
2018-04-13 14:50:13 -0400

    add icons and count to top  bar

--- src/components/GameHeader.js
+++ src/components/GameHeader.js
@@ -4,13 +4,16 @@ import { Container, Image } from 'semantic-ui-react';
 const GameHeader = () => {
   return (
     <Container className="game-header">
-      <Image className="game-header" src="/assets/images/player_card.png" size="small" />
-      <Image src="/assets/images/player_card.png" size="small" />
-      <Image src="/assets/images/player_card.png" size="small" />
-      <Image src="/assets/images/player_card.png" size="small" />
-      <Image src="/assets/images/player_card.png" size="small" />
-      <Image src="/assets/images/player_card.png" size="small" />
-      <Image src="/assets/images/player_card.png" size="small" />
+      <Container className="game-header-container">
+        <Image src="/assets/images/player_card.png" size="mini" /><div>53</div>
+        <Image src="/assets/images/cube_blue.png" size="mini" /><div>20</div>
+        <Image src="/assets/images/cube_yellow.png" size="mini" /><div>20</div>
+        <Image src="/assets/images/cube_black.png" size="mini" /><div>20</div>
+        <Image src="/assets/images/cube_red.png" size="mini" /><div>20</div>
+        <Image src="/assets/images/station.png" size="mini" /><div>10</div>
+        <Image src="/assets/images/infection_rate.png" size="mini" /><div>10</div>
+        <Image src="/assets/images/outbreaks.png" size="mini" /><div>10</div>
+      </Container>
     </Container>
   );
 };

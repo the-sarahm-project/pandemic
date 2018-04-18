f2f80a8064f2f8b83a79111b62b51bc41d8be1a1
Sam Chai <sam.chai0501@gmail.com>
2018-04-18 11:10:51 -0400

    load cube count, research stations, infection rate, and num outbreaks from firestore

--- src/components/GameHeader.js
+++ src/components/GameHeader.js
@@ -12,13 +12,13 @@ const GameHeader = (props) => {
         <Container className="game-header-container">
           <Image src="/assets/images/empty_card.png" size="mini" />
           <Image src="/assets/images/player_card.png" size="mini" /><div>{playerDeck.length}</div>
-          <Image src="/assets/images/cube_blue.png" size="mini" /><div>20</div>
-          <Image src="/assets/images/cube_yellow.png" size="mini" /><div>20</div>
-          <Image src="/assets/images/cube_black.png" size="mini" /><div>20</div>
-          <Image src="/assets/images/cube_red.png" size="mini" /><div>20</div>
-          <Image src="/assets/images/station.png" size="mini" /><div>10</div>
-          <Image src="/assets/images/infection_rate.png" size="mini" /><div>10</div>
-          <Image src="/assets/images/outbreaks.png" size="mini" /><div>10</div>
+          <Image src="/assets/images/cube_blue.png" size="mini" /><div>{game.blueDiseaseCubes}</div>
+          <Image src="/assets/images/cube_yellow.png" size="mini" /><div>{game.yellowDiseaseCubes}</div>
+          <Image src="/assets/images/cube_black.png" size="mini" /><div>{game.blackDiseaseCubes}</div>
+          <Image src="/assets/images/cube_red.png" size="mini" /><div>{game.redDiseaseCubes}</div>
+          <Image src="/assets/images/station.png" size="mini" /><div>{game.remainingResearchStations}</div>
+          <Image src="/assets/images/infection_rate.png" size="mini" /><div>{game.infectionRate}</div>
+          <Image src="/assets/images/outbreaks.png" size="mini" /><div>{game.numOutbreaks}</div>
           <Image src="/assets/images/infection_card.png" size="mini" />
           <Image src="/assets/images/empty_card.png" size="mini" />
         </Container>

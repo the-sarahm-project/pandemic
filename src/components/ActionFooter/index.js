2943ee085c159ef9074bebb6190cb8692d05f268
Sam Chai <sam.chai0501@gmail.com>
2018-04-27 12:50:00 -0400

    move ActionFooter to index

--- src/components/ActionFooter/index.js
+++ src/components/ActionFooter/index.js
@@ -1,4 +1,23 @@
-export { default as ActionFooter } from './ActionFooter';
+import React from 'react';
+import { Sidebar } from 'semantic-ui-react';
+import { Move, Build, Share, Treat, Cure } from './index';
+
+const ActionFooter = () => {
+  return (
+    <Sidebar className="action-footer" direction="bottom" visible={true} width="very wide">
+      <div className="action-container">
+        <Move />
+        <Build />
+        <Share />
+        <Treat />
+        <Cure />
+      </div>
+    </Sidebar>
+  );
+};
+
+export default ActionFooter;
+
 export { default as Build } from './Build';
 export { default as Cure } from './Cure';
 export { default as Move } from './Move';

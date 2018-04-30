c8df8f205db7122cd5250a59b43a2dc794e4c81a
Sam Chai <sam.chai0501@gmail.com>
2018-04-29 20:02:38 -0400

    comment out setuplogic and init

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,14 @@ import { compose } from 'redux';
 import '../App.css';
 import { Sidebar, Board, ActionFooter } from './index';
 import { doc } from '../utils';
-import { db } from '../store';
-import { init, setupLogic } from '../utils';
+// import { db } from '../store';
+// import { init, setupLogic } from '../utils';
 
 class App extends Component {
   componentDidMount() {
-    const numPlayers = 4, difficultyLevel = 4;
+    // const numPlayers = 4, difficultyLevel = 4;
 
-    setupLogic(db.collection('games').doc(doc), numPlayers, difficultyLevel);
+    // setupLogic(db.collection('games').doc(doc), numPlayers, difficultyLevel);
     // init(db, numPlayers, difficultyLevel);
   }
 

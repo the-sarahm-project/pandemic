684ae16122d6ade7e6b495040e9c8b27db4c4332
Sam Chai <sam.chai0501@gmail.com>
2018-05-01 16:45:03 -0400

    fix Move test

--- src/components/ActionFooter/Move.test.js
+++ src/components/ActionFooter/Move.test.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
-import { firestoreConnect } from 'react-redux-firebase';
 import { Move, mapStateToProps } from './Move';
-import { shallowWithStore } from 'enzyme-redux';
-import store from '../../store';
+import { dummyState } from '../../utils';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Move', () => {
   it('receives correct props from store', () => {
     const expectedProps = {
-      dispatch: () => {},
-      firebase: {},
-      firestore: {},
       currentTurn: 1,
       neighbors: []
     };
-    const ConnectedComponent = compose(
-      firestoreConnect(),
-      connect(mapStateToProps)
-    )(Move);
-    const component = shallowWithStore(<ConnectedComponent />, store);
-    expect(Object.keys(component.dive().props()).sort()).toEqual(Object.keys(expectedProps).sort());
+    expect(Object.keys(mapStateToProps(dummyState)).sort()).toEqual(Object.keys(expectedProps).sort());
   });
 
   const component = shallow(<Move />);

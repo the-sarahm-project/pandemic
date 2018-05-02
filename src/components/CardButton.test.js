aee44964af5e3351d393966bb3ede46f2929a356
Sam Chai <sam.chai0501@gmail.com>
2018-05-02 19:45:15 -0400

    add eslint and CardButton test

---
+++ src/components/CardButton.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import CardButton from './CardButton';
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('CardButton', () => {
+  const setSelectedAndActive = jest.fn();
+  const component = shallow(<CardButton card={[]} active={false} setSelectedAndActive={setSelectedAndActive} />);
+
+  it('executes setSelectedAndActive when clicked', () => {
+    const mockEvent = {
+      currentTarget: {
+        value: ''
+      }
+    };
+    component.find('Button').simulate('click', mockEvent);
+    expect(setSelectedAndActive).toHaveBeenCalledWith('', []);
+  });
+
+  describe('renders', () => {
+    it('a Button', () => {
+      expect(component.find('Button').name()).toBe('Button');
+    });
+
+    it('a Button with a div', () => {
+      expect(component.find('Button').childAt(0).name()).toBe('div');
+    });
+
+    it('a Button with a div with an Image', () => {
+      expect(component.find('Button').childAt(0).childAt(0).name()).toBe('Image');
+    });
+  });
+});

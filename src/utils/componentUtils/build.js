73b37b91bfe6add68be26cee3ad8f53967371ee4
Sam Chai <sam.chai0501@gmail.com>
2018-05-05 11:15:39 -0400

    add error handling

--- src/utils/componentUtils/build.js
+++ src/utils/componentUtils/build.js
@@ -5,49 +5,66 @@ export const setCityResearchStation = async (firestore, currentTurn, currentCity
     console.log('Not enough cards selected');
     return;
   }
-  try {
-    const game = await getGame(firestore);
-    const currentCityRef = game.ref.collection('cities').doc(currentCity.id);
-    const currentPlayerSnapshot = await getCurrentPlayerSnapshot(game, currentTurn, currentCityRef);
-    await setResearchStationTrue(currentCityRef);
-    await updateCurrentHand(currentPlayerSnapshot, unusedCityCards, currentCity);
-    await removeCards(cardsToRemove);
-  } catch(err) {
-    console.log(err);
-  }
+  const game = await getGame(firestore);
+  const currentCityRef = game.ref.collection('cities').doc(currentCity.id);
+  const currentPlayerSnapshot = await getCurrentPlayerSnapshot(game, currentTurn, currentCityRef);
+  await setResearchStationTrue(currentCityRef);
+  await updateCurrentHand(currentPlayerSnapshot, unusedCityCards, currentCity);
+  await removeCards(cardsToRemove);
 };
 
 async function getGame(firestore) {
-  return await firestore.get(`games/${doc}`);
+  try {
+    return await firestore.get(`games/${doc}`);
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 async function getCurrentPlayerSnapshot(game, currentTurn, currentCityRef) {
   const currentTurnRef = game.ref.collection('players').doc(`${currentTurn}`);
   setResearchStationTrue(currentCityRef);
   setRemainingResearchStations(game);
-  return await currentTurnRef.get();
+  try {
+    return await currentTurnRef.get();
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 //build research station
 async function setResearchStationTrue(currentCityRef) {
-  return await currentCityRef.update({ researchStation: true });
+  try {
+    return await currentCityRef.update({ researchStation: true });
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 async function setRemainingResearchStations(game) {
   const remainingResearchStations = game.data().remainingResearchStations;
-  return await game.ref.update({ remainingResearchStations: remainingResearchStations - 1 });
+  try {
+    return await game.ref.update({ remainingResearchStations: remainingResearchStations - 1 });
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 async function removeCards(cardsToRemove) {
   //remove cards from unusedCityCards
-  await Promise.all(cardsToRemove.map(card => card.delete()));
-  // for (let card of cardsToRemove) {
-  //   await card.delete();
-  // }
+  try {
+    await Promise.all(cardsToRemove.map(card => card.delete()));
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 async function updateCurrentHand(currentPlayerSnapshot, unusedCityCards, currentCity) {
   const currentHand = currentPlayerSnapshot.data().currentHand;
   const newCurrentHand = currentHand.filter(card => unusedCityCards[card.id].color !== currentCity.color);
-  await currentPlayerSnapshot.ref.update({ currentHand: newCurrentHand });
+  try {
+    await currentPlayerSnapshot.ref.update({ currentHand: newCurrentHand });
+  } catch(err) {
+    console.log(err);
+  }
 }

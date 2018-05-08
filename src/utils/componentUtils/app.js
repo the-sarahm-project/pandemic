54c1c69073510a4eca00a8909d6f9a3cc9274ad2
Sam Chai <sam.chai0501@gmail.com>
2018-05-07 20:47:01 -0400

    add console log to know when done setting up

--- src/utils/componentUtils/app.js
+++ src/utils/componentUtils/app.js
@@ -6,4 +6,5 @@ export const initAndSetupGame = async (numPlayers, difficultyLevel, create) => {
   const newDoc = await init(db, numPlayers, difficultyLevel);
   console.log(newDoc);
   await setupLogic(db.collection('games').doc(newDoc), numPlayers, difficultyLevel);
+  console.log('done!');
 };

6ab562af41325e85d16cf7919c6612dd33db7e9c
Sam Chai <sam.chai0501@gmail.com>
2018-05-07 20:45:43 -0400

    fix Build test after swapping Cure and Build

--- src/components/ActionFooter/Build.test.js
+++ src/components/ActionFooter/Build.test.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { Build, mapStateToProps } from './Build';
-import { dummyState } from '../../utils';
+import Build from './Build';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Build', () => {
-  it('receives correct props from mapStateToProps', () => {
-    const expectedProps = ['currentTurn', 'buildDisabled', 'currentCity', 'unusedCityCards', 'sameColorCityCards'];
-    expect(Object.keys(mapStateToProps(dummyState))).toEqual(expect.arrayContaining(expectedProps));
-  });
 
-  describe('renders a Choose Card Modal', () => {
+  const component = shallow(<Build />);
 
-    const component = shallow(<Build />);
-
-    it('ChooseCardModal', () => {
-      expect(component.find('ChooseCardModal').name()).toBe('ChooseCardModal');
-    });
-
-    it('that takes in the correct props', () => {
-      const expectedProps = ['ModalTrigger', 'disabled', 'cards', 'action'];
-      expect(Object.keys(component.props())).toEqual(expect.arrayContaining(expectedProps));
-    });
+  it('renders a Button', () => {
+    expect(component.find('Button').name()).toBe('Button');
   });
 });

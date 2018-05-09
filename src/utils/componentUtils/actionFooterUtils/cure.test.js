539428ae80e0da85e0f1da76daeadbfba4e14a7a
Sam Chai <sam.chai0501@gmail.com>
2018-05-09 18:01:54 -0400

    add test for cureButtonDisabled

--- src/utils/componentUtils/actionFooterUtils/cure.test.js
+++ src/utils/componentUtils/actionFooterUtils/cure.test.js
@@ -61,8 +61,80 @@ describe('build', () => {
   });
 
   describe('cureButtonDisabled', () => {
-    it('does something', () => {
-      expect(2).toEqual(2);
+    const curedFalse = {
+      blueCureMarker: false
+    };
+    const curedTrue = {
+      blueCureMarker: true
+    };
+    const currentCityBlue = {
+      color: 'blue'
+    };
+    const currentHandBlue = [
+      {id: 'Atlanta', color: 'blue'},
+      {id: 'Paris', color: 'blue'},
+      {id: 'Lima', color: 'yellow'},
+      {id: 'Madrid', color: 'blue'},
+      {id: 'London', color: 'blue'},
+      {id: 'Essen', color: 'blue'}
+    ];
+    const currentHandYellow = [
+      {id: 'Santiago', color: 'yellow'},
+      {id: 'Miami', color: 'yellow'},
+      {id: 'Lima', color: 'yellow'},
+      {id: 'LosAngeles', color: 'yellow'},
+      {id: 'Bogota', color: 'yellow'},
+      {id: 'Essen', color: 'blue'}
+    ];
+    const unusedCityCards = {
+      Atlanta: {
+        color: 'blue'
+      },
+      Paris: {
+        color: 'blue'
+      },
+      Madrid: {
+        color: 'blue'
+      },
+      London: {
+        color: 'blue'
+      },
+      Essen: {
+        color: 'blue'
+      },
+      Lima: {
+        color: 'yellow'
+      },
+      Santiago: {
+        color: 'yellow'
+      },
+      Miami: {
+        color: 'yellow'
+      },
+      LosAngeles: {
+        color: 'yellow'
+      },
+      Bogota: {
+        color: 'yellow'
+      }
+    };
+    describe('Disabled', () => {
+      it(`is disabled when that color is already cured`, () => {
+        const disabled = cureButtonDisabled(curedTrue, currentCityBlue, currentHandBlue, unusedCityCards);
+        expect(disabled).toEqual(true);
+      });
+
+      it('is disabled when there are not enough cards that match currentCity color', () => {
+        const disabled = cureButtonDisabled(curedFalse, currentCityBlue, currentHandYellow, unusedCityCards);
+        expect(disabled).toEqual(true);
+      });
+    });
+
+    describe('Enabled', () => {
+      it(`is enabled when there are enough cards of the current city's color and that color is not cured`, () => {
+        const disabled = cureButtonDisabled(curedFalse, currentCityBlue, currentHandBlue, unusedCityCards);
+        expect(disabled).toEqual(false);
+      });
     });
   });
 

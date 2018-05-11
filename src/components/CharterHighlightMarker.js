d367a7ffe2199fe2cc1a465bad8185fdb6e8b8e7
Sarah Zhao <sarahzhao25@gmail.com>
2018-05-10 22:52:35 -0400

    charter flight initialized

---
+++ src/components/CharterHighlightMarker.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { firestoreConnect, isLoaded } from 'react-redux-firebase';
+
+const CharterHighlightMarker = (props) => {
+  return (
+    <div>hi</div>
+  );
+};

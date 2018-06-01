f568a57a312dd07f1a4435dd66fb76eb9ddf8864
Sam Chai <sam.chai0501@gmail.com>
2018-06-01 19:06:58 -0400

    fix to use async await, and await all

--- src/utils/componentUtils/actionFooterUtils/build.js
+++ src/utils/componentUtils/actionFooterUtils/build.js
@@ -8,21 +8,21 @@ export const buildResearchStation = async (firestore, currentCityId, currentTurn
     const currentPlayerSnapshot = await getPlayerRef(game, `${currentTurn}`).get();
     const cardToRemove = currentPlayerSnapshot.data().currentHand.filter(card => card.id === currentCityId);
     // set research station to true for that city.
-    currentCityRef.update({ researchStation: true });
+    await currentCityRef.update({ researchStation: true });
     // update remaining research stations count
-    setRemainingResearchStations(game);
+    await setRemainingResearchStations(game);
     // remove card from current hand
-    updateCurrentHand(currentPlayerSnapshot, cardToRemove);
+    await updateCurrentHand(currentPlayerSnapshot, cardToRemove);
     //remove card from unusedCityCards
-    removeCards(cardToRemove);
+    await removeCards(cardToRemove);
   } catch(err) {
     console.log(err);
   }
 };
 
-export const setRemainingResearchStations = game => {
+export const setRemainingResearchStations = async game => {
   const remainingResearchStations = game.data().remainingResearchStations;
-  return game.ref.update({ remainingResearchStations: remainingResearchStations - 1 });
+  return await game.ref.update({ remainingResearchStations: remainingResearchStations - 1 });
 };
 
 export const buildButtonDisabled = (remainingResearchStations, currentHand, currentCityId) => {

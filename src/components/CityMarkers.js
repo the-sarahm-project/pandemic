b8e4f813e6f725fb941f4072719a1efe36cb2acd
Sam Chai <sam.chai0501@gmail.com>
2018-06-03 23:15:32 -0400

    update CityMarkers test and add DiseaseCube test

--- src/components/CityMarkers.js
+++ src/components/CityMarkers.js
@@ -8,20 +8,22 @@ import { iconContainer, getCities, getDiseaseCubes, getCubeCoords } from '../uti
 
 export const CityMarkers = ({ cities }) => {
   return (
-    isLoaded(cities) && Object.values(cities).map(city => {
-      const cubes = getDiseaseCubes(city);
-      return (
-        <div key={city.coords}>
-          <Marker position={city.coords} icon={iconContainer[city.color]} />
-          {city.researchStation ? <ResearchStation coords={city.coords} /> : null}
-          {cubes.map(cube => {
-            const [color, num] = cube;
-            const cubeCoords = getCubeCoords(city.coords, num, cubes.length);
-            return <DiseaseCube key={cubeCoords} color={color} coords={cubeCoords} />;
-          })}
-        </div>
-      );
-    })
+    <div>
+      {isLoaded(cities) && Object.values(cities).map(city => {
+        const cubes = getDiseaseCubes(city);
+        return (
+          <div key={city.coords}>
+            <Marker position={city.coords} icon={iconContainer[city.color]} />
+            {city.researchStation && <ResearchStation coords={city.coords} researchStation={city.researchStation} />}
+            {cubes.map(cube => {
+              const [color, num] = cube;
+              const cubeCoords = getCubeCoords(city.coords, num, cubes.length);
+              return <DiseaseCube key={cubeCoords} color={color} coords={cubeCoords} />;
+            })}
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
--- src/components/CityMarkers.test.js
+++ src/components/CityMarkers.test.js
@@ -8,22 +8,25 @@ Enzyme.configure({ adapter: new Adapter() });
 describe('CityMarkers', () => {
   const component = shallow(<CityMarkers />);
   component.setProps({cities: dummyState.firestore.data.games[doc].cities});
-
   it('receives correct props from mapStateToProps', () => {
     const expectedProps = ['cities'];
     expect(Object.keys(mapStateToProps(dummyState))).toEqual(expect.arrayContaining(expectedProps));
-    expect(Object.keys(mapStateToProps(dummyState).cities).length).toEqual(48);
   });
 
-  it('renders a div', () => {
-    expect(component.find('div').length).toEqual(1);
+  it('renders 48 divs for each city', () => {
+    expect(component.children().every('div')).toBe(true);
+    expect(component.children().length).toEqual(48);
   });
 
-  it ('renders the correct number of Markers', () => {
+  it('renders the correct number of Markers', () => {
     expect(component.find('Marker').length).toEqual(48);
   });
 
-  it ('renders the correct number of Research Stations', () => {
+  it('renders the correct number of Research Stations', () => {
     expect(component.find('ResearchStation').length).toEqual(2);
+  });
+
+  it('renders the correct number of Disease Cubes', () => {
+    expect(component.find('DiseaseCube').length).toEqual(5);
   });
 });

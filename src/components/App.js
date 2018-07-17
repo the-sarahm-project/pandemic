62ad7ab717f76b9bdd92d9186bf2b98268702421
Sam Chai <sam.chai0501@gmail.com>
2018-07-17 16:12:57 -0400

    moved conditional logic into Game instead of App

--- src/components/App.js
+++ src/components/App.js
@@ -2,36 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import '../App.css';
 import { Game, HomeScreen } from './index';
-import { db } from '../store';
-import history from '../history';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      gameExists: false
-    };
-  }
-
-  async componentWillMount() {
-    const gameId = history.location.pathname.slice(1);
-    const game = gameId && await db.collection('games').doc(gameId).get();
-    if (game.exists || !gameId) {
-      this.setState({ gameExists: true });
-    } else {
-      history.push('/');
-    }
-  }
-
-  render() {
-    return (
-
-      <Switch>
-        <Route exact path="/" component={HomeScreen} />
-        {this.state.gameExists && <Route exact path="/:gameid" component={Game} />}
-      </Switch>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={HomeScreen} />
+      <Route exact path="/:gameid" component={Game} />
+    </Switch>
+  );
+};
 
 export default App;
--- src/components/Game.js
+++ src/components/Game.js
@@ -3,17 +3,38 @@ import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
 import { withRouter } from 'react-router-dom';
 import { SidebarCards, Board, ActionFooter } from './index';
+import { db } from '../store';
 import history from '../history';
 
-const Game = () => {
-  return (
-    <div className="game">
-      <SidebarCards />
-      <Board />
-      <ActionFooter />
-    </div>
-  );
-};
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameExists: false
+    };
+  }
+
+  async componentWillMount() {
+    const gameId = history.location.pathname.slice(1);
+    const game = gameId && await db.collection('games').doc(gameId).get();
+    if (game.exists) {
+      this.setState({ gameExists: true });
+    } else {
+      history.push('/');
+    }
+  }
+
+  render() {
+    return (
+      this.state.gameExists &&
+        <div className="game">
+          <SidebarCards />
+          <Board />
+          <ActionFooter />
+        </div>
+    );
+  }
+}
 
 export default withRouter(compose(
   firestoreConnect(() => [
